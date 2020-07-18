007a6f263d9193be92465b27dc8382fbae820bda
bouaik <bouaik.lhou@outlook.fr>
2020-07-18 14:51:34 +0100

    add faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do 
+    Course.create!([{
+        title: Faker::Educator.course_name,
+        description: Faker::TvShows::GameOfThrones.quote
+    }])
+end
