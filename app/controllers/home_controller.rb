2ecde0d4804ebf91819a7666bc3f24a29f8aa965
bouaik <bouaik.lhou@outlook.fr>
2020-07-19 03:26:45 +0100

    style course paths

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
       @courses = Course.all.limit(3)
       @recent_courses = Course.all.limit(3).order(created_at: :desc)
