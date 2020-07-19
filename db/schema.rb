6fec231575814efead37e4ccae53664267d311c3
bouaik <bouaik.lhou@outlook.fr>
2020-07-19 18:26:21 +0100

    add fields to course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_122615) do
+ActiveRecord::Schema.define(version: 2020_07_19_164339) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 2020_07_19_122615) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.string "slug"
+    t.text "short_description"
+    t.string "language", default: "English", null: false
+    t.string "level", default: "Beginner", null: false
+    t.integer "price", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
+# User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
 
 30.times do 
     Course.create!([{
         title: Faker::Educator.course_name,
         description: Faker::TvShows::GameOfThrones.quote,
-        user_id: User.first.id
+        user_id: User.first.id,
+        short_description: Faker::Quote.famous_last_words,
+        language: Faker::ProgrammingLanguage.name,
+        level: "beginner",
+        price: Faker::Number.between(from: 1000, to: 20000)
     }])
 end
