e36860a0c7a706ac9bb3e3511bf76d0da0284d93
bouaik <bouaik.lhou@outlook.fr>
2020-07-19 05:00:36 +0100

    fix database 2

--- db/migrate/20200718144423_add_user_to_courses.rb
+++ db/migrate/20200718144423_add_user_to_courses.rb
@@ -1,5 +1,5 @@
 class AddUserToCourses < ActiveRecord::Migration[6.0]
   def change
-    add_reference :courses, :user, foreign_key: true
+    add_reference :courses, :user,null: false, foreign_key: true
   end
 end
