4c2d0d0715b762443dfd368ece8f82d3e97d5eef
bouaik <bouaik.lhou@outlook.fr>
2020-07-20 14:40:30 +0100

    add ransack gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
+User.create!(email: 'admin@admin.com', password: 'admin@admin.com', password_confirmation: 'admin@admin.com')
 
 30.times do 
     Course.create!([{
