a66824826c2fb4c0a813f6ff2c14ab8612d9a0b9
bouaik <bouaik.lhou@outlook.fr>
2020-07-20 16:20:40 +0100

    add search fields for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,7 +7,9 @@ class CoursesController < ApplicationController
     if params[:title]
       @courses = Course.where('title LIKE ?', "%#{params[:title]}%") #case-insensitive
     else
-      @courses = Course.all
+      # @courses = Course.all
+      @q = Course.ransack(params[:q])
+      @courses = @q.result(distinct: true)
     end
   end
 
