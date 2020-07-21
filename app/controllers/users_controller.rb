9b8e8c63b1f3c8af8ebd4fdc524b48fa2095da32
bouaik <bouaik.lhou@outlook.fr>
2020-07-21 16:25:15 +0100

    edit roles for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update]
 
     def index
         # @users = User.all.order(created_at: :desc)
@@ -7,4 +8,25 @@ class UsersController < ApplicationController
         @users = @q.result(distinct: true)
     end
 
+    def edit
+    end
+
+    def update
+        if @user.update(users_params)
+            redirect_to users_path, notice: "User #{@user.email} was successfully updates"
+        else
+            render :edit
+        end
+    end
+
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def users_params
+       params.require(:user).permit({role_ids: []})
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users
+  resources :users, only: [:index, :edit, :show, :update]
   root 'home#index'
   get 'home/activity'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
