744f2fbbb6d7a7bcb12cdfbc348dbcbd4dc2c7a9
bouaik <bouaik.lhou@outlook.fr>
2020-07-21 17:51:02 +0100

    add pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
         @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) # navbar_search
     end
 
+    include Pundit
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+
+    def user_not_authorized # pundit
+        flash[:alert] = "You are not authorized to perform this action."
+        redirect_to(request.referrer || root_path)
+    end
 end
