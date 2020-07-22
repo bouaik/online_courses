16b1aca1db06c278c63542ed77ecc981a3de9b87
bouaik <bouaik.lhou@outlook.fr>
2020-07-22 01:15:24 +0100

    add lessons to the course card

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ApplicationRecord
     validates :description, presence: true, length: { minimum: 5 }
 
     belongs_to :user
-    has_many :lessons
+    has_many :lessons, dependent: :destroy
 
     has_rich_text :description
 
