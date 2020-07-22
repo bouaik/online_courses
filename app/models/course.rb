9cbc15065f568218506dd92b82e3af8762c125fb
bouaik <bouaik.lhou@outlook.fr>
2020-07-22 12:20:57 +0100

    add public activity to lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -22,4 +22,8 @@ class Course < ApplicationRecord
 
     include PublicActivity::Model
     tracked owner: Proc.new{ |controller, model| controller.current_user }
+    
+    def to_s
+        title
+    end
 end
