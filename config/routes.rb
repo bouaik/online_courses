2da30f6b11b6d99c3d065c77b2eeacb354695be3
bouaik <bouaik.lhou@outlook.fr>
2020-07-25 15:00:53 +0100

    add teacher navigation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index, :edit, :show, :update]
   root 'home#index'
-  get 'home/activity'
+  get 'activity', to: "home#activity"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
