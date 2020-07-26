7505d6351c5dbe2ba6296422c237e3df0f65edb7
bouaik <bouaik.lhou@outlook.fr>
2020-07-26 19:20:15 +0100

    personalize home page and make it dynamic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,11 @@ class HomeController < ApplicationController
     @courses = Course.all.limit(3)
     @recent_courses = Course.all.limit(3).order(created_at: :desc)
     @enrollements = Enrollement.reviewd.order(rating: :desc , created_at: :desc).limit(3)
-  end
 
+    @top_rated_courses = Course.order(average_rating: :desc, created_at: :desc).limit(3)
+    @popular_courses = Course.order(enrollements: :desc, created_at: :desc).limit(3)
+    @purchased_courses = Course.joins(:enrollements).where(enrollements: {user: current_user}).order(created_at: :desc).limit(3)
+  end
   def activity
     @activities = PublicActivity::Activity.all
   end
