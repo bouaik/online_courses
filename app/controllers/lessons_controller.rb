388ec620b4ad81c6745171dbcfcd2a454be96619
bouaik <bouaik.lhou@outlook.fr>
2020-07-28 03:02:28 +0100

    add navigation to lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,6 +12,7 @@ class LessonsController < ApplicationController
   def show
     authorize @lesson
     current_user.view_lesson(@lesson)
+    @lessons = @course.lessons
   end
 
   # GET /lessons/new
