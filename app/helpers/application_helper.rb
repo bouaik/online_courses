d84c695a3620f1ba96fe85d4df7067f20b37c869
bouaik <bouaik.lhou@outlook.fr>
2020-07-29 01:29:17 +0100

    style activity key with font awsome icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def crud_label(key)
+        case key 
+            when "create" 
+                "<i class='fa fa-plus'></i>".html_safe
+            when "update"
+                "<i class='fa fa-pen'></i>".html_safe
+            when "destroy"
+                "<i class='fa fa-trash'></i>".html_safe
+        end
+    end
 end
